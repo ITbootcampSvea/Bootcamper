d1946e4ba170c14037b691b5e054fb3f5927af95
MilosRistic1987 <risticmilos1987@gmail.com>
2020-06-28 11:49:15 +0200

    circular progress bar switch

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -7,58 +7,97 @@ import NavBar from '../components/NavBar/NavBar';
 
 
 export default function Home() {
-    const percentage = 20;
+    const studentTest = [
+        {
+            testName: 'Test1',
+            percentage: 20,
+            id: 1
+        },
+        {
+            testName: 'Test2',
+            percentage: 43,
+            id: 2
+        },
+        {
+            testName: 'Test3',
+            percentage: 62,
+            id: 3
+        },
+        {
+            testName: 'Test4',
+            percentage: 89,
+            id: 4
+        },
+    ]
+    const defaultCircleStyle = {
+        textColor: '#161515',
+        trailColor: '#161515',
+        backgroundColor: '#fff',
+    }
+
+    const applyProgressStyle = (percentage) => {
+        console.warn(percentage)
+        var progressStyle;
+        switch (true) {
+            case (percentage <= 20): progressStyle = {
+                ...defaultCircleStyle,
+                pathColor: 'rgb(184, 78, 57)'
+            }
+                break;
+
+
+
+            case (percentage > 20 && percentage <= 50): progressStyle = {
+                ...defaultCircleStyle,
+                pathColor: 'rgb(255,152,3)'
+            }
+                break;
+
+
+            case (percentage > 50 && percentage <= 80): progressStyle = {
+                ...defaultCircleStyle,
+                pathColor: '#307bd1'
+            }
+                break;
+
+            case (percentage > 80 && percentage <= 100): progressStyle = {
+                ...defaultCircleStyle,
+                pathColor: '#6AB93C'
+            }
+                break;
+
+            default:
+                progressStyle = {
+                    ...defaultCircleStyle,
+                    pathColor: '#afb1b3'
+                }
+
+        }
+
+        return progressStyle;
+    }
+
 
     return (<>
         <NavBar></NavBar>
         <div className='wrapper'>
             <Container fluid className='aligmentFlexCenter' >
                 <Row>
-                    <Col className="col-xl-3">
-                        <Card className='card' style={{ width: '22rem' }}>
-                            <Card.Header className='text-center progresCardHeader cardHeaderBackground'>Test1</Card.Header>
-                            <Card.Body>
-                                <CircularProgressbar styles={buildStyles({
-                                    textColor: '#161515',
-                                    trailColor:'#161515' ,
-                                    backgroundColor: '#fff',
-                                    pathColor:'rgb(255,152,3)'
-                                })} value={percentage} text={`${percentage}%`} strokeWidth={14} background={true} />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col className="col-xl-3">
-                        <Card className='card' style={{ width: '22rem' }}>
-                            <Card.Header className='text-center progresCardHeader cardHeaderBackground'>Test2</Card.Header>
-                            <Card.Body>
-                                <CircularProgressbar value={percentage * 2} text={`${percentage * 2}%`} />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col className="col-xl-3">
-                        <Card className='card' style={{ width: '22rem' }}>
-                            <Card.Header className='text-center progresCardHeader cardHeaderBackground'>Test3</Card.Header>
-                            <Card.Body>
-                                <CircularProgressbar styles={buildStyles({
-                                    textColor: '#161515',
-                                    trailColor:'#161515' ,
-                                    backgroundColor: '#fff',
-                                    pathColor:'#6AB93C'
-                                })} value={percentage * 4.5} text={`${percentage * 4.5}%`}  strokeWidth={14} background={true}/>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col className=".col-xl-3">
-                        <Card className='card' style={{ width: '22rem' }}>
-                            <Card.Header className='text-center progresCardHeader cardHeaderBackground'>Test4</Card.Header>
-                            <Card.Body>
-                                <CircularProgressbar value={percentage * 3.6} text={`${percentage * 3.6}%`} />
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                    {studentTest.map(el =>
+                        <Col className="col-xl-3" key={el.id} test={el}>
+                            <Card className='card' style={{ width: '22rem' }}>
+                                <Card.Header className='text-center progresCardHeader cardHeaderBackground'>{el.testName}</Card.Header>
+                                <Card.Body>
+                                    <CircularProgressbar styles={buildStyles(applyProgressStyle(el.percentage))} value={el.percentage} text={`${el.percentage}%`} strokeWidth={14} background={true} />
+                                </Card.Body>
+                            </Card>
+                  )
+                    </Col>)}
+
                 </Row>
             </Container>
         </div>
     </>
+
     )
 }
