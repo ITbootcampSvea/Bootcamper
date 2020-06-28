0e0ef3d05db5cc714e6104ee7ebe3cd6dadb4d6d
mmplanic <mmplanic@gmail.com>
2020-06-28 14:29:46 +0200

    initial login page/form structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Redirect } from "react-router-dom";
 // import StudentRoute from "./routes/StudentRoute";
 import PublicRoute from "./routes/PublicRoute";
 import Home from './views/Home/Home';
+import Login from './views/Login/Login';
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       <Switch>
 
       <PublicRoute component={Home} path="/home" />
-{/* 
-        <PublicRoute component={LogIn } path="/login" />
+      <PublicRoute component={Login} path="/login" />
 
+{/* 
+        
         <PrivateRoute component={Home} path="/home" />
         <PrivateRoute component={CreateOrderItem} path="/order/:id" />
         <PrivateRoute component={CreatePoll} path="/createpoll" />
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Form,Button, Card} from 'react-bootstrap';
 
 
 const Login = () =>{
 
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+  
+      setValidated(true);
+    };
+  
+
 return(
-<div className='wrapper'>
-    <h1>Login</h1>
-</div>
+    <Card style={{ width: '18rem' }}>
+        <Card.Header>
+            Please log in
+        </Card.Header>
+        <Card.Body>      
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+
+                <Form.Group controlId="formBasicEmail" >
+                    <Form.Label>User name</Form.Label>
+                    <Form.Control pattern=".{3,15}" required type="text" placeholder="Username" />
+                    <Form.Control.Feedback type="invalid">
+                    User name must be 3-15 characters long.
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control pattern=".{6,15}" required type="password" placeholder="Password" />
+                    <Form.Control.Feedback type="invalid">
+                    Password must be 6-15 characters long.
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    LOGIN
+                </Button>
+            </Form>
+        </Card.Body>  
+        </Card>
+
 )
     
 
