b783b275d96221880f84b2e88ba061899019bf81
MilosRistic1987 <risticmilos1987@gmail.com>
2020-06-30 22:29:33 +0200

    update

--- src/views/Students/Students.jsx
+++ src/views/Students/Students.jsx
@@ -4,7 +4,7 @@ import { Container, Card, Row, Col, ListGroup } from 'react-bootstrap';
 
 
 const Students = () => {
-    const studentBase = [{ name: "Milos", lastName: 'Ristic' }, { name: "Pera", lastName: 'Ristic' }, { name: "Jovana", lastName: 'Maksimovic' }, { name: "Zorana", lastName: 'Kec' }, { name: "Milos", lastName: 'Djordjevic' }, { name: "Pavle", lastName: 'Njegus' }, { name: "Leposava", lastName: 'Prodanovic' }, { name: "Marija", lastName: 'Milovanovic' }, { name: "Mihajlo", lastName: 'Popovic' }, { name: "Marija", lastName: 'Strugar' }, { name: "Bogdan", lastName: 'Knezevic' }, { name: "Jugoslav", lastName: 'Colovic' }, { name: "Bojana", lastName: 'Rilak' }, { name: "Ana", lastName: 'Majstorovic' },{ name: "Ksenija", lastName: 'Lukic'},{ name: "Dejan", lastName: 'Cvetkovic'},{ name: "Ana", lastName: 'Popadic'},{ name: "Tamara", lastName: 'Rodic'},{ name: "Zvezdan", lastName: 'Gladisev'}]
+    const studentBase = [{ name: "Milos", lastName: 'Ristic' }, { name: "Pera", lastName: 'Ristic' }, { name: "Jovana", lastName: 'Maksimovic' }, { name: "Zorana", lastName: 'Kec' }, { name: "Milos", lastName: 'Djordjevic' }, { name: "Pavle", lastName: 'Njegus' }, { name: "Leposava", lastName: 'Prodanovic' }, { name: "Marija", lastName: 'Milovanovic' }, { name: "Mihajlo", lastName: 'Popovic' }, { name: "Marija", lastName: 'Strugar' }, { name: "Bogdan", lastName: 'Knezevic' }, { name: "Jugoslav", lastName: 'Colovic' }, { name: "Bojana", lastName: 'Rilak' }, { name: "Ana", lastName: 'Majstorovic' }, { name: "Ksenija", lastName: 'Lukic' }, { name: "Dejan", lastName: 'Cvetkovic' }, { name: "Ana", lastName: 'Popadic' }, { name: "Tamara", lastName: 'Rodic' }, { name: "Zvezdan", lastName: 'Gladisev' }]
 
 
     return (
@@ -14,14 +14,16 @@ const Students = () => {
                 <Row>
                     <Col xl={5} className='aligmentFlexCenter'>
                         <ListGroup className='listContainer' >
+
                             {studentBase.map(el => <ListGroup.Item student={el}>
                                 <div className='aligmentFlexCenter'>  <span className='indicator'></span></div>
                                 <h4 className={'listItem'}>{el.name} {el.lastName}</h4>
                                 <ul className='absence'>
-                                    <li><i className="fa fa-check-circle "></i></li>
-                                    <li><i className="fa fa-check-circle"></i></li>
-                                    <li><i className="fa fa-check-circle"></i></li>
-                                    <li><i className="fa fa-times-circle"></i></li>
+                                    <li><i className="fa fa-star"></i></li>
+                                    <li><i className="fa fa-star"></i></li>
+                                    <li><i className="fa fa-star"></i></li>
+                                    <li><i className="fa fa-star"></i></li>
+                                    <li><i className="fa fa-star-o"></i></li>
                                 </ul>
                             </ListGroup.Item>)}
                         </ListGroup>
