5475958a20cc6489e88c33d59fa89b92ad4190dc
Danko Novovic <danko.novovic@gmail.com>
2020-07-06 11:20:08 +0200

    search bar added home

--- src/views/Home/Dashboard/DashCard/DashCard.jsx
+++ src/views/Home/Dashboard/DashCard/DashCard.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React from "react";
+import { Card } from "react-bootstrap";
 
 export default function DashCard(props) {
-    const {singleCard} = props; 
+  let { singleCard } = props;
 
-    
-    let numText = 50;
-    let numTitle = 10;
-    
-    
-    let title = '';
-    if (singleCard.title.length <= numTitle) {
-        title = singleCard.title;
-    } else {
-        title = singleCard.title.slice(0, numTitle) + '...';
-    }
+  let numText = 50;
+  let numTitle = 10;
 
-    let text = '';
-    if (singleCard.text.length <= numText) {
-        text = singleCard.text;
-    } else {
-        text = singleCard.text.slice(0, numText) + '...';
-    }
-      
+  let title = "";
+  if (singleCard.title.length <= numTitle) {
+    title = singleCard.title;
+  } else {
+    title = singleCard.title.slice(0, numTitle) + "...";
+  }
 
-    return (
-        <div>
-            <p>{singleCard.time}</p>
-            <h2>{title}</h2>
-            <p>{text}</p>
-        </div>
-    )
+  let text = "";
+  if (singleCard.text.length <= numText) {
+    text = singleCard.text;
+  } else {
+    text = singleCard.text.slice(0, numText) + "...";
+  }
+
+  return (
+    <div>
+      <p>{singleCard.time}</p>
+      <h2>{title}</h2>
+      <p>{text}</p>
+    </div>
+  );
 }
--- src/views/Home/Dashboard/DashCardModal/DashCardModal.jsx
+++ src/views/Home/Dashboard/DashCardModal/DashCardModal.jsx
@@ -1,31 +1,27 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
 
 export default function DashCardModal(props) {
+  const { singlemodal } = props;
 
-    const {singlemodal} = props; 
-    
-
-    return (
-        <Modal
-          {...props}
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              {singlemodal.title}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>
-              {singlemodal.text}
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={props.onHide}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      );
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {singlemodal.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{singlemodal.text}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
