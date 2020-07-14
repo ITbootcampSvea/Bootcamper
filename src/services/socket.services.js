9ba581ed0a84777dc0d0f9eb3320fffa50dcd781
mmplanic <mmplanic@gmail.com>
2020-07-14 22:18:15 +0200

    socket implementation in progress

--- src/services/socket.services.js
+++ src/services/socket.services.js
@@ -2,7 +2,7 @@ import socketIO from 'socket.io-client';
 
 const baseURL = "http://localhost:4000";
 
-
+ 
 
 export default function startSocketConnection(){
     const socket = socketIO.connect(baseURL);
@@ -11,6 +11,14 @@ export default function startSocketConnection(){
         console.log("socket connected");
     })
     
+    socket.on('newdashcard',(data)=>{
+        console.log("response socket", data);
+        //update redux state
+    })
+    socket.on('editeddashcard',(data)=>{
+        console.log("response socket", data);
+        //update redux state
+    })
     socket.on('newdashcard',(data)=>{
         console.log("response socket", data);
         //update redux state
